77b8f502c098bb8b5d1cf4a4e8e3bf440cf9dfc4
mat-gro <m.grosso@tugraz.at>
2020-06-30 15:35:49 +0200

    Adding RDM user creation for pure admin user

--- invenio_rdm_pure/initial_setup.py
+++ invenio_rdm_pure/initial_setup.py
@@ -8,15 +8,15 @@ full_path = f"{dirpath}/{folder_name}"
 
 # data_setup parameters
 import_setup = {
-    "pure_api_key": "Pure API key",
-    "pure_password": "Pure API password",
-    "pure_rest_api_url": "Pure API url",
-    "pure_username": "Pure username",
-    "rdm_host_url": "RDM host URL",
-    "rdm_token": "RDM token",
+    # "pure_api_key": "Pure API key",
+    # "pure_password": "Pure API password",
+    # "pure_rest_api_url": "Pure API url",
+    # "pure_username": "Pure username",
+    # "rdm_host_url": "RDM host URL",
+    # "rdm_token": "RDM token",
     "email_sender": "Sender e-mail for Pure file deletion",
-    "email_sender_password": "Sender password for Pure file deletion",
-    "email_receiver": "E-mail of responsible for Pure deletion",
+    # "email_sender_password": "Sender password for Pure file deletion",
+    # "email_receiver": "E-mail of responsible for Pure deletion",
 }
 
 # If the folder does not exist then creates it
@@ -30,3 +30,9 @@ for file in import_setup:
     value = input(f"{import_setup[file]}: ")
     # Create file
     open(f"{dirpath}/{folder_name}/{file}.txt", "w+").write(value)
+
+# Create Pure user and assign admin rights
+email = input("RDM user creation - Insert desired Pure user e-mail: ")
+password = input("RDM user creation - Password: ")
+os.system(f"pipenv run invenio users create {email} --password {password} --active")
+os.system("pipenv run invenio roles add {email} admin")
