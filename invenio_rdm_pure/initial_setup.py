8f0f508a0ecf98e3c8ab3f5999c92cfdd704fea6
mat-gro <m.grosso@tugraz.at>
2020-07-14 15:11:39 +0200

    Creating pure user txt files

--- invenio_rdm_pure/initial_setup.py
+++ invenio_rdm_pure/initial_setup.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from setup import pure_rdm_user_file, pure_rdm_password_file
 
 dirpath = os.path.dirname(os.path.abspath(__file__))
 
@@ -8,16 +9,16 @@ full_path = f"{dirpath}/{folder_name}"
 
 # data_setup parameters
 import_setup = {
-    "pure_api_key": "Pure API key",
-    "pure_rest_api_url": "Pure API URL",
-    "pure_username": "Pure username",
-    "pure_password": "Pure password",
-    "rdm_host_url": "RDM host URL",
-    "rdm_token": "RDM token",
-    "db_host": "Database host",
-    "email_sender": "Pure file deletion - Sender e-mail",
-    "email_sender_password": "Pure file deletion - Sender password",
-    "email_receiver": "Pure file deletion - E-mail of Pure responsible",
+    # "pure_api_key": "Pure API key",
+    # "pure_rest_api_url": "Pure API URL",
+    # "pure_username": "Pure username",
+    # "pure_password": "Pure password",
+    # "rdm_host_url": "RDM host URL",
+    # "rdm_token": "RDM token",
+    # "db_host": "Database host",
+    # "email_sender": "Pure file deletion - Sender e-mail",
+    # "email_sender_password": "Pure file deletion - Sender password",
+    # "email_receiver": "Pure file deletion - E-mail of Pure responsible",
 }
 
 # If the folder does not exist then creates it
@@ -30,10 +31,17 @@ for file in import_setup:
     # Ask for input value
     value = input(f"{import_setup[file]}: ")
     # Create file
-    open(f"{dirpath}/{folder_name}/{file}.txt", "w+").write(value)
+    open(f"{dirpath}/{folder_name}/{file_name}.txt", "w+").write(value)
 
-# Create Pure user and assign admin rights
+# Create Pure user in RDM
+# User email
 email = input("RDM user creation - Insert desired Pure user e-mail: ")
+open(pure_rdm_user_file, "w+").write(email)
+# User password
 password = input("RDM user creation - Password: ")
+open(pure_rdm_password_file, "w+").write(password)
+# Create user
 os.system(f"pipenv run invenio users create {email} --password {password} --active")
+# Assign admin rights
 os.system("pipenv run invenio roles add {email} admin")
+
