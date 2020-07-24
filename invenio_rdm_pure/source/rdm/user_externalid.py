55f1021bc88de1880ea65b424d7fb1716396c47e
mat-gro <m.grosso@tugraz.at>
2020-07-24 08:40:23 +0200

    Debugging logged in user with no externalId

--- invenio_rdm_pure/source/rdm/user_externalid.py
+++ invenio_rdm_pure/source/rdm/user_externalid.py
@@ -7,5 +7,6 @@ def user_externalid():
     if current_user.is_authenticated:
         id = current_user.get_id()
         user_external = UserIdentity.query.filter_by(id_user=id).first()
-        return user_external.id
+        if user_external:
+            return user_external.id
     return False
