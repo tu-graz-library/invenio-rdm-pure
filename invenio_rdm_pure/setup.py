1c86661d0373658d282e3292ffcfe566b9c737ed
mb <mb_wali@hotmail.com>
2020-08-04 14:38:05 +0200

    path as default

--- invenio_rdm_pure/setup.py
+++ invenio_rdm_pure/setup.py
@@ -4,7 +4,7 @@ import os
 dirpath = os.path.dirname(os.path.abspath(__file__))
 
 # data_setup_path = f"{dirpath}/data_setup"
-data_setup_path = f"{dirpath}/../../june/data_setup"
+data_setup_path = f"{dirpath}/data_setup"
 
 pure_uuid_length = 36
 
