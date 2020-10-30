2e9d6721edb038be19aea397bd6a42967184e2f4
Benjamin Gaal <benjamin.gaal@tugraz.at>
2020-10-30 13:29:50 +0100

    config.py Added docstyle formatting

--- invenio_rdm_pure/config.py
+++ invenio_rdm_pure/config.py
@@ -5,7 +5,7 @@
 # invenio-rdm-pure is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-"""Invenio module that adds pure"""
+"""Invenio module that adds pure."""
 
 # TODO: This is an example file. Remove it if your package does not use any
 # extra configuration variables.
@@ -96,39 +96,39 @@ RDM_RECORDS_METADATA_NAMESPACES = {
 
 # Invenio-RDM-Pure
 INVENIO_PURE_CONFIG_PRESENT = False
-""" Helper variable to check the presence of pure configuration. 
-    If the values are present and correct in invenio.cfg 
-    this variable should be set to True. """
+"""Helper variable to check the presence of pure configuration. 
+   If the values are present and correct in invenio.cfg 
+   this variable should be set to True."""
 
 ## Invenio
 INVENIO_PURE_HOST_URL = "https://127.0.0.1:5000/"
-""" URL of invenio host """
+"""URL of invenio host."""
 
 INVENIO_PURE_RECORD_URL = INVENIO_PURE_HOST_URL + "api/records/{}"
-""" Endpoint to access single a record """
+"""Endpoint to access single a record."""
 
 INVENIO_PURE_RECORDS_URL = INVENIO_PURE_HOST_URL + "api/records"
-""" Endpoint to access multiple records."""
+"""Endpoint to access multiple records."""
 
 INVENIO_PURE_USER_EMAIL = ""
-""" Email of user creating the records """
+"""Email of user creating the records."""
 
 INVENIO_PURE_USER_PASSWORD = ""
-""" Password of user creating the records """
+"""Password of user creating the records."""
 
 
 ## Pure
 PURE_API_URL = ""
-""" URL of the Pure Instance's REST API """
+"""URL of the Pure Instance's REST API."""
 
 PURE_API_KEY = ""
-""" Token/Key of the Pure Instance's REST API """
+"""Token/Key of the Pure Instance's REST API."""
 
 PURE_USERNAME = ""
-""" Username of Pure user having the necessary permissions to acquire needed Pure entries """
+"""Username of Pure user having the necessary permissions to acquire needed Pure entries."""
 
 PURE_PASSWORD = ""
-""" Password of Pure user having the necessary permissions to acquire needed Pure entries """
+"""Password of Pure user having the necessary permissions to acquire needed Pure entries."""
 
 PURE_RESPONSIBLE_EMAIL = ""
-""" Email address of Pure user having the necessary permissions to delete Pure entries """
+"""Email address of Pure user having the necessary permissions to delete Pure entries."""
