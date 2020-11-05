8d6fcd1cfbb53dca7acd9394b30988bed20dc3a1
Benjamin Gaal <benjamin.gaal@tugraz.at>
2020-11-05 15:30:48 +0100

    global Added __init__.py files

---
+++ invenio_rdm_pure/source/rdm/run/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 Technische Universität Graz.
+#
+# invenio-rdm-pure is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio module that adds pure."""
+
+__all__ = ("__version__", "InvenioRdmPure")
