db13c0ea36c465581a109c07a81a732f594e0254
Benjamin Gaal <benjamin.gaal@tugraz.at>
2020-12-10 09:25:55 +0100

    test(run-tests.sh): modify test execution

--- run-tests.sh
+++ run-tests.sh
@@ -6,13 +6,10 @@
 # invenio-rdm-pure is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-docker-services-cli up postgresql es redis && \
-pydocstyle invenio_rdm_pure tests docs && \
-isort --check-only --diff --recursive invenio_rdm_pure tests && \
-check-manifest --ignore ".*-requirements.txt" && \
-sphinx-build -qnNW docs docs/_build/html && \
-pytest
-
+python -m check_manifest --ignore ".*-requirements.txt"
+python -m sphinx.cmd.build -qnNW docs docs/_build/html
+docker-services-cli --verbose up es postgresql redis
+python -m pytest
 tests_exit_code=$?
 docker-services-cli down
 exit "$tests_exit_code"
