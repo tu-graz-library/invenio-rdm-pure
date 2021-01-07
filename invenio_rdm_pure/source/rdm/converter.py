71f3e45959898943b5f0d2b45d3333d564aa0fcb
Benjamin Gaal <benjamin.gaal@tugraz.at>
2021-01-07 16:07:15 +0100

    feature(converter): add attribute conversion methods.

--- invenio_rdm_pure/source/rdm/converter.py
+++ invenio_rdm_pure/source/rdm/converter.py
@@ -18,6 +18,113 @@ class Converter(object):
         """Default Constructor of the class."""
         pass
 
-    def convert_metadata(self, output_format: MetadataFormat, input_data: dict) -> dict:
-        """Converts metadata from/to given format."""
+    def convert_pure_json_to_marc21_xml(self, pure_json: dict):
+        """Convert record from Pure JSON format to MARC21XML."""
+        record = Marc21Record()
+        for attribute, value in pure_json.items():
+            self.convert_attribute(attribute, value, record)
+        record.print_to_xml()
+
+    def convert_attribute(self, attribute: str, value: object, record: Marc21Record):
+        """Description."""
+        convert_function = getattr(self, f"convert_{attribute}", lambda *args: None)
+        convert_function(value, record)
+        if type(value) is dict:
+            for subattribute, subvalue in value.items():
+                self.convert_attribute(subattribute, subvalue, record)
+
+    def convert_abstract(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_additionalLinks(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_bibliographicalNote(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_edition(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_electronicIsbns(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_event(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_isbns(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_journalAssociation(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_journalNumber(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_keywordGroups(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_languages(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_managingOrganisationalUnit(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_numberOfPages(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_organisationalUnits(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_pages(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_patentNumber(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_peerReview(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_placeOfPublication(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_publicationSeries(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_publicationStatuses(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_publisher(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_subTitle(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_title(self, value: str, record: Marc21Record):
+        """Description."""
+        pass
+
+    def convert_volume(self, value: str, record: Marc21Record):
+        """Description."""
         pass
