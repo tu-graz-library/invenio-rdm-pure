4ad6dc0ddce2bb59723105b79f46ba3a31294f0a
Benjamin Gaal <benjamin.gaal@tugraz.at>
2021-01-08 11:56:53 +0100

    docs(converter): add meaningful docstrings to methods

--- invenio_rdm_pure/source/rdm/converter.py
+++ invenio_rdm_pure/source/rdm/converter.py
@@ -26,7 +26,7 @@ class Converter(object):
         record.print_to_xml()
 
     def convert_attribute(self, attribute: str, value: object, record: Marc21Record):
-        """Description."""
+        """Traverse dictionary and extract necessary attributes."""
         convert_function = getattr(self, f"convert_{attribute}", lambda *args: None)
         convert_function(value, record)
         if type(value) is dict:
@@ -34,97 +34,97 @@ class Converter(object):
                 self.convert_attribute(subattribute, subvalue, record)
 
     def convert_abstract(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the abstract to the Marc21Record."""
         pass
 
     def convert_additionalLinks(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the abstract attribute to the Marc21Record."""
         pass
 
     def convert_bibliographicalNote(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the bibliographicalNote attribute to the Marc21Record."""
         pass
 
     def convert_edition(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the edition attribute to the Marc21Record."""
         pass
 
     def convert_electronicIsbns(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the electronicIsbns attribute to the Marc21Record."""
         pass
 
     def convert_event(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the event attribute to the Marc21Record."""
         pass
 
     def convert_isbns(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the isbns attribute to the Marc21Record."""
         pass
 
     def convert_journalAssociation(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the journalAssociation attribute to the Marc21Record."""
         pass
 
     def convert_journalNumber(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the journalNumber attribute to the Marc21Record."""
         pass
 
     def convert_keywordGroups(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the keywordGroups attribute to the Marc21Record."""
         pass
 
     def convert_languages(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the languages attribute to the Marc21Record."""
         pass
 
     def convert_managingOrganisationalUnit(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the managingOrganisationalUnit attribute to the Marc21Record."""
         pass
 
     def convert_numberOfPages(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the numberOfPages attribute to the Marc21Record."""
         pass
 
     def convert_organisationalUnits(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the organisationalUnits attribute to the Marc21Record."""
         pass
 
     def convert_pages(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the pages attriute to the Marc21Record."""
         pass
 
     def convert_patentNumber(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the patentNumber attribute to the Marc21Record."""
         pass
 
     def convert_peerReview(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the peerReview attribute to the Marc21Record."""
         pass
 
     def convert_placeOfPublication(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the placeOfPublication attribute to the Marc21Record."""
         pass
 
     def convert_publicationSeries(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the publicationSeries attribute to the Marc21Record."""
         pass
 
     def convert_publicationStatuses(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the publicationStatuses attribute to the Marc21Record."""
         pass
 
     def convert_publisher(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the publisher attribute to the Marc21Record."""
         pass
 
     def convert_subTitle(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the subTitle attribute to the Marc21Record."""
         pass
 
     def convert_title(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the title attribute to the Marc21Record."""
         pass
 
     def convert_volume(self, value: str, record: Marc21Record):
-        """Description."""
+        """Add the volume attribute to the Marc21Record."""
         pass
