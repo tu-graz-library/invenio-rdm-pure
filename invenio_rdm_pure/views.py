e98239ae5eb83fc6ebe2277be9d6bbfd5a523969
Benjamin Gaal <benjamin.gaal@tugraz.at>
2021-03-30 10:09:33 +0200

    modification(views): remove unused view

--- invenio_rdm_pure/views.py
+++ invenio_rdm_pure/views.py
@@ -12,21 +12,9 @@ import os
 from flask import Blueprint
 from flask_babelex import gettext as _
 
-from .setup import dirpath, pure_import_file
-
 blueprint = Blueprint(
     "invenio_rdm_pure",
     __name__,
     template_folder="templates",
     static_folder="static",
 )
-
-
-@blueprint.route("/pure_import_xml")
-def index1():
-    """Render pure_import_xml view."""
-    # Check if the XML file does not exist
-    if not os.path.isfile(pure_import_file):
-        # Run pure_import task to create the XML file
-        os.system(f"python {dirpath}/cli.py pure_import")
-    return open(pure_import_file, "r").read()
