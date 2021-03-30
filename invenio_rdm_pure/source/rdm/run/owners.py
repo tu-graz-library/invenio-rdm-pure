e962c0e89da0a855b2ee480977703fb5fe2f700e
Benjamin Gaal <benjamin.gaal@tugraz.at>
2021-03-30 09:44:13 +0200

    docs(copyright): change copyright year to 2021

--- invenio_rdm_pure/source/rdm/run/owners.py
+++ invenio_rdm_pure/source/rdm/run/owners.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2020 Technische Universität Graz
+# Copyright (C) 2021 Technische Universität Graz
 #
 # invenio-rdm-pure is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
